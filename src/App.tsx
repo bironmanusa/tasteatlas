42bf8e72442e64f5d2a5146d36e33aeb3d307218
Mateo Mayorga <103150863+bironmanusa@users.noreply.github.com>
2025-07-30 19:28:44 +0000

    Taller 3

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Student from './components/Student'
 
 // PENDIENTE: Cree la interfaz
 import { type Dish } from './interface/Dish'
+import { useState, useEffect } from 'react'
 
 
 function App() {
@@ -13,13 +14,18 @@ function App() {
   let url = "https://raw.githubusercontent.com/aavendan/datos/refs/heads/main/tasteatlas/bestdishes100-2425.json"
   
   // PENDIENTE: Variable de estado y la función de modificación. 
-  
+  const [dishes, setDishes] = useState<Dish[]>([])
 
 
   // PENDIENTE: 
   // Realizar una petición asíncrona a la URL. La respuesta es un JSON. 
   // Al recibir la respuesta, actualice la variable de estado.
-
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then((data: Dish[]) => setDishes(data))
+      .catch(err => console.error(err))
+  }, [url])
   
 
 
@@ -30,7 +36,7 @@ function App() {
         <Grid size={{ xs: 12 }}>
 
           {/* PENDIENTE: Envíe sus datos (apellidos, nombres y paralelo) como props del componente */}
-          <Student></Student>
+          <Student apellidos="Mayorga Navarro" nombres="Mateo Sebastian" paralelo="2"></Student>
 
         </Grid>
         
@@ -38,7 +44,7 @@ function App() {
         <Grid size={{ xs: 12 }}>
 
           {/* PENDIENTE: Envíe la variable de estado como prop */}
-          <DishTable data={  }></DishTable>
+          <DishTable data={dishes}></DishTable>
         
         </Grid>
         
---
+++ src/interface/Dish.tsx
@@ -0,0 +1,9 @@
+export interface Dish {
+    position: string;
+    title: string;
+    subtitle: string;
+    country: string;
+    rating: number;
+    iconic: string;
+    ingredients: string;
+}
